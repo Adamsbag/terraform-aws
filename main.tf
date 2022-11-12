f73a420e9954f0ec98ba54ab724cac0b76d128ce
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2022-11-12 14:10:22 -0500

    AWS networking begins

--- main.tf
+++ main.tf
@@ -1,23 +1,92 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-provider "aws" {
-  region = "us-west-2"
+resource "aws_subnet" "public0" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+resource "aws_subnet" "private0" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_eip" "nat0" {
+  vpc = true
+}
+
+resource "aws_eip" "nat1" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main0" {
+  allocation_id = aws_eip.nat0.id
+  subnet_id     = aws_subnet.public0.id
+}
+
+resource "aws_nat_gateway" "main1" {
+  allocation_id = aws_eip.nat1.id
+  subnet_id     = aws_subnet.public1.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
+}
+
+resource "aws_route_table" "private0" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main0.id
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main1.id
+  }
+}
+
+resource "aws_route_table_association" "public0" {
+  subnet_id      = aws_subnet.public0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
 }
