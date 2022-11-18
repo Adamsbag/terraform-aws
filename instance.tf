6713a562efb833ff0728905d5beedc49953718f2
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2022-11-17 19:08:10 -0500

    Added 2 ec2 instances Public-private

---
+++ instance.tf
@@ -0,0 +1,75 @@
+resource "aws_instance" "public" {
+  ami                         = "ami-0b0dcb5067f052a63"
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  key_name                    = "DevOpsKP"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.env_code}"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["73.172.1.229/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami = "ami-0b0dcb5067f052a63"
+
+  instance_type          = "t3.micro"
+  key_name               = "DevOpsKP"
+  vpc_security_group_ids = [aws_security_group.private.id]
+  subnet_id              = aws_subnet.private[1].id
+
+  tags = {
+    Name = "${var.env_code}"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Allow VPC traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
