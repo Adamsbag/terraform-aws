05afaece76db0600cd3f8d659250997376ba6f04
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2022-11-27 16:19:12 -0500

    Provision AWS ec2 by adding a User Data

--- instance.tf
+++ instance.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "public" {
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public[0].id
 
+  user_data = file("user-data.sh")
+
   tags = {
     Name = "${var.env_code}"
   }
@@ -31,6 +33,14 @@ resource "aws_security_group" "public" {
   name        = "${var.env_code}-public"
   description = "Allow inbound traffic"
   vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["73.172.1.229/32"]
+  }
 
   ingress {
     description = "SSH from public"
